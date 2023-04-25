39fa22fc52d59be7bf3450902c4e72862cb2978a
Gir0fa <99766910+wbrous@users.noreply.github.com>
2023-04-24 20:21:24 -0500

    add comments

--- password.py
+++ password.py
@@ -13,41 +13,41 @@
 
 import time
 
-pass_type = 1
-revealed_letters = ""
-tries = 0
+pass_type = 1 # Change to "0" if u wanna change password.
+revealed_letters = "" # Don't change. It is for the revealing letters. Line 49.
+tries = 0 # For stopping code once u run out of guesses.
 
-print("Made by Gir0fa#6723")
+print("Made by Gir0fa#6723") # Credits
 
 # Get passfile. (Create file if needed).
 
-if pass_type == 0:
-    with open("pass.txt", "w") as file:
-        password = input("Enter new password: ")
-        file.write(password)
-        exit(f"Set password to {password}")
-elif pass_type == 1:
-    with open("pass.txt", "r") as file:
-        password = file.read()
+if pass_type == 0: # check var from line 16
+    with open("pass.txt", "w") as file: # open file
+        password = input("Enter new password: ") # ask for pass
+        file.write(password) # write pass
+        exit(f"Set password to {password}") # confirmation
+elif pass_type == 1: # check var
+    with open("pass.txt", "r") as file: # read file
+        password = file.read() #define local var from text
 else:
-    exit("ERROR: set pass_type in config to 1!!!")
+    exit("ERROR: set pass_type in config to 1!!!") # if error
 
 # Main Code
 
-try:
-    for letter in password:
+try: # if error
+    for letter in password: # for loop
 
-        revealed_letters += letter
-        tries += 1
+        revealed_letters += letter # some math
+        tries += 1 # more math
 
-        guess = input(f"Guess #{len(revealed_letters)}? ")
+        guess = input(f"Guess #{len(revealed_letters)}? ") # ask for guess
 
-        if guess == password:
+        if guess == password: # if guess is pass
             print(f"You got it after {tries} tries. \nThe word was {password}.")
             break
         else:
             print(f"Wrong! Revealed letters are: {revealed_letters}")
-except:
+except: # run if error
     print(
     """
     Made by Gir0fa#6723
